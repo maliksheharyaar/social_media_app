d18e5e668ea8efeea46e7dd96a475d5531cbbcd9
Sheharyaar <38083700+maliksheharyaar@users.noreply.github.com>
2022-04-03 03:12:21 -0400

    [SBA-2]:Initalizaing Redux and Server routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import social_blog from './images/social_blog.png'; 
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
     return(
-        <div>
-            <h1>App Loaded</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Social Blog</Typography>
+                <img className={classes.image} src={social_blog} alt='memories' height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
